094d1a81998956ce28da5c72b3cd9f0e3e07355f
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-09-28 23:12:23 +0200

    first steps in setting up PostgreSQL

--- bin/database.sh
+++ bin/database.sh
@@ -16,9 +16,35 @@ export LANG="en_US.UTF8"
 export LC_ALL="en_US.UTF8"
 
 #
-# setup PostgrSQL
+# install PostgrSQL
 #
 
 sudo apt-get --assume-yes install postgresql
 
+#
+# edit settings for access to PostgreSQL
+#
 
+# /etc/postgresql/8.4/main/pg_hba.conf
+# local   all         all                               trust
+# host    all         all         127.0.0.1/32          trust
+# host    all         all         ::1/128               trust
+sudo sed -i 's/^\(local\|host\)\( * all * all .* \)\([A-Za-z0-9]*\)$/\1\2trust/' /etc/postgresql/8.4/main/pg_hba.conf
+
+#
+# Done!, now restart
+#
+
+sudo service postgresql-8.4 reload
+
+#
+# setup PostgreSQL for Act!
+#
+
+sudo -u postgres createdb act           --encoding=UTF8
+sudo -u postgres createdb acttest       --encoding=UTF8
+
+psql -h localhost -U postgres act     < ${INSTALLER}/etc/dbinit
+psql -h localhost -U postgres acttest < ${INSTALLER}/etc/dbinit
+
+# END
